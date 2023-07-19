d88b5b82147ca43dc7d39f9653eaf4738c65ee19
wan <hubhy@nate.com>
2023-07-19 15:01:08 +0900

    오후 수업 중간까지

--- src/main/java/shop/mtcoding/mall/controller/ProductController.java
+++ src/main/java/shop/mtcoding/mall/controller/ProductController.java
@@ -1,11 +1,20 @@
 package shop.mtcoding.mall.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import shop.mtcoding.mall.model.ProductRepository;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Controller
-public class ProductController{
+public class ProductController {
+
+    @Autowired
+    private ProductRepository productRepository;
 
     @GetMapping("/")
     public String home() {
@@ -18,11 +27,15 @@ public class ProductController{
         return "write";
     }
 
-    @PostMapping ("/product") // 우리가 파싱안하고 스프링에서 알아서 파싱을 해준다
-    public void write(String name, int price, int qty){
-        System.out.println("name: "+name);
-        System.out.println("price: "+price);
-        System.out.println("qty: "+qty);
+    @PostMapping("/product") // 우리가 파싱안하고 스프링에서 알아서 파싱을 해준다
+    public void write(String name, int price, int qty, HttpServletResponse response) throws IOException {
+        System.out.println("name: " + name);
+        System.out.println("price: " + price);
+        System.out.println("qty: " + qty);
+
+        productRepository.save(name, price, qty);
+        response.sendRedirect("/");
+
     }
 
 }
--- src/main/java/shop/mtcoding/mall/model/ProductRepository.java
+++ src/main/java/shop/mtcoding/mall/model/ProductRepository.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import javax.transaction.Transactional;
 
 
@@ -15,8 +16,13 @@ public class ProductRepository {
     @Autowired // DBconnection -> 스프링에 만들어진거를 땡겨온다
     private EntityManager em;
 
-    @Transactional
-    public void insert(Product product) {
-        em.persist(product);
+    @Transactional // 자원누유를 막는 어노테이션 close역할
+    public void save(String name, int price, int qty){
+        Query query = em.createNativeQuery("insert into product_tb(name, price, qty) values(:name, :price, :qty)");
+        // Query는 버퍼임
+        query.setParameter("name", name);
+        query.setParameter("price", price);
+        query.setParameter("qty", qty);
+        query.executeUpdate();
     }
 }
